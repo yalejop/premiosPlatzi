eafd49a7446df9ff477f0f9e40993c1d9bda3989
Alejandro Cataño Herrera <alejo.9211@hotmail.com>
2022-08-20 10:27:55 -0500

    create template home and handler 404 error

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='details'),
+    path('<int:question_id>/results/', views.result, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='votes'),
+    
 ]
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-# Create your views here.
+
+from .models import Question
 
 def index(request):
-    return HttpResponse('Estás en la página principal de Premios Platzi App')
+    latest_question_list = Question.objects.all()
+    
+    return render(request, 'polls/index.html', {
+        'latest_question_list': latest_question_list
+    })
 
 def detail(request, question_id):
-    return HttpResponse(f'Estas viendo la pregunta número {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {
+        'question': question
+    })
 
 def result(request, question_id):
     return HttpResponse(f'Estas viendo los resultados de la pregunta número {question_id}')
