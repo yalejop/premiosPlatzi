ba8d85459bd8e54be4e7efad039a85a6bcdaa841
Alejandro Cataño Herrera <alejo.9211@hotmail.com>
2022-08-20 09:57:26 -0500

    create views for the app

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World')
+    return HttpResponse('Estás en la página principal de Premios Platzi App')
+
+def detail(request, question_id):
+    return HttpResponse(f'Estas viendo la pregunta número {question_id}')
+
+def result(request, question_id):
+    return HttpResponse(f'Estas viendo los resultados de la pregunta número {question_id}')
+
+def vote(request, question_id):
+    return HttpResponse(f'Estas votando a la pregunta número {question_id}')
