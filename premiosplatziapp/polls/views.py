1aa240cccdab6f6a64a63bc04efdb88654a8b7e3
Alejandro Cataño Herrera <alejo.9211@hotmail.com>
2022-08-21 08:14:35 -0500

    create the result view

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.all()
@@ -17,7 +18,21 @@ def detail(request, question_id):
     })
 
 def result(request, question_id):
-    return HttpResponse(f'Estas viendo los resultados de la pregunta número {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {
+        'question': question
+    })
 
 def vote(request, question_id):
-    return HttpResponse(f'Estas votando a la pregunta número {question_id}')
+    question =get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',{
+            'question': question,
+            'error_message': 'No elegiste una respuesta'
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
